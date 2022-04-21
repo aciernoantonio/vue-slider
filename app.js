19ffd07d6b88095e93d1a5d13626ab57c5e712be
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-21 15:54:48 +0200

    add milestone 2

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ const app = new Vue({
 
        places: [
         {
-            image: 'img/01.jpg',
+            image: './assets/img/01.jpg',
             title: 'Svezia',
             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
         },
         {
-            image: 'img/02.jpg',
+            image: './assets/img/02.jpg',
             title: 'Svizzera',
             text: 'Lorem ipsum.',
         },
         {
-            image: 'img/03.jpg',
+            image: './assets/img/03.jpg',
             title: 'Gran Bretagna',
             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
         },
         {
-            image: 'img/04.jpg',
+            image: './assets/img/04.jpg',
             title: 'Germania',
             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
         },
         {
-            image: 'img/05.jpg',
+            image: './assets/img/05.jpg',
             title: 'Paradise',
             text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
         }
